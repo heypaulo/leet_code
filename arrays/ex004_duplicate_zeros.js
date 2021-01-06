77c056ee01d18b0df2371c3a40f0ac110ca96efe
Paulo da Silva <paulodasilva@hey.com>
2021-01-06 18:31:15 -0500

    Code Refactored

--- arrays/ex004_duplicate_zeros.js
+++ arrays/ex004_duplicate_zeros.js
@@ -17,15 +17,13 @@ Explanation: After calling your function, the input array is modified to: [1,2,3
 
 */
 const duplicateZeros = function(arr) {
-  const arrayLength = arr.length;
-	for (let i = 0; i < arrayLength; i++) {
-		if(arr[i] === 0) {
-			arr.splice(i, 0 ,0)
+  for (let i= 0; i < arr.length; i++) {
+		if (arr[i] === 0) {
+			arr.splice(i, 0, 0);
 			i++
+			arr.pop();
 		}
 	}
-  arr.splice(arrayLength, (arr.length - arrayLength))
-  
   return arr;
 }
 
--- arrays/ex005_merge_sorted_array.js
+++ arrays/ex005_merge_sorted_array.js
@@ -20,22 +20,11 @@ nums2.length == n
 
 */
 
-var merge = function(nums1, m, nums2, n) {
+const merge = function(nums1, m, nums2, n) {
+  nums1.splice(m, nums1.length, ...nums2);
+  nums1.sort((a, b) => a - b);
 
-  m--;
-  n--;
-  let i = nums1.length - 1;
-
-  while (i >= 0) {
-    if (n > -1 && m > -1 && nums1[m] >= nums2[n]) {
-      nums1[i] = nums1[m];
-      nums1[m] = nums2[n];
-      m--;
-    }
-    else if (n > -1) {
-      nums1[i] = nums2[n--];
-    }
-    i--;
-  }
-
+  return nums1;
 };
+
+console.log(merge([1,2,3,0,0,0], 3, [2, 5, 6], 3));
